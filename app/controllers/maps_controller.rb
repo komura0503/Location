be87bf5b54cc67a6a0b397ec6fff3cc285e40064
supernemutai <tokikake66@gmail.com>
2021-11-14 21:09:54 +0000

    [update] 地図情報

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,9 +1,9 @@
 class MapsController < ApplicationController
-  
+
   def new
     @map = Map.new
   end
-  
+
   def create
     @map = Map.new(map_params)
     @map.post_id = current_user.id
@@ -14,9 +14,9 @@ class MapsController < ApplicationController
   def index
     @maps = Map.all
   end
-  
+
   private
-  
+
   def map_params
     params.require(:map).permit(:address, :latitude, :longitude)
   end
