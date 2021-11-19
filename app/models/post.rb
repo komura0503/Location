83ed1b465fd9485ae307b6c63fd48f9ee9b750ee
supernemutai <tokikake66@gmail.com>
2021-11-19 16:54:30 +0000

    [add] 検索機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,13 @@ class Post < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
-  def self.search(keyword)
-    where(["title likes? OR body likes?", "%#{keyword}%", "%#{keyword}%"])
+
+  def self.search(search)
+    if search
+      Post.where(["title LIKE ?", "%#{search}%"])
+    else
+      Post.all
+    end
   end
 
   attachment :image
