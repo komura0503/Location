0890b9b750e3afb403e381903afbda726bfc66cb
supernemutai <tokikake66@gmail.com>
2021-11-28 01:48:05 +0000

    [update] レイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,6 @@ class User < ApplicationRecord
 
   attachment :profile_image
 
-  validates :name, presence: true
+  validates :name, presence: true, length: {maximum: 15 }
+  validates :introduction, length: {maximum: 70 }
 end
