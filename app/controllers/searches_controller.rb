d87ff0cc6ba0413a7d28033e1b497f521d5249d5
supernemutai <tokikake66@gmail.com>
2022-01-14 14:58:40 +0000

    [update] ページネーション修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
   def search
     @search = params[:search]
     @posts = Post.search(params[:search])
-    @post_pages = Post.page(params[:page]).reverse_order
+    @post_pages = @posts.page(params[:page]).reverse_order
     @tag_list = Tag.all
   end
 
